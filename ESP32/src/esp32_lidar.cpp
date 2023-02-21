80aff8bf93c768a1894a314719adca837953e7a9
Chri <21258355+christiansassi@users.noreply.github.com>
2023-02-21 23:41:50 +0100

    Add files via upload

---
+++ ESP32/src/esp32_lidar.cpp
@@ -0,0 +1,52 @@
+#include "esp32_lidar.h"
+#include "esp32_uart.h"
+
+//*****************************************************************************
+//
+//! Initializates the lidar sensor.
+//
+//*****************************************************************************
+void LidarClass::init()
+{
+    if(!this->lox.begin())
+    {
+        Serial.println("Failed to init ");
+        Serial.print(LIDAR_NAME);
+        Serial.print(" sensor.");
+
+        while(1)
+        delay(100);
+    }
+}
+
+//*****************************************************************************
+//
+//! Retrieves the detected measure from the sensor.
+//! It returns -1 if out of range.
+//
+//*****************************************************************************
+float LidarClass::getMeasureLidar()
+{
+    this->lox.rangingTest(&(this->measure), false);
+
+    return (this->measure.RangeStatus != 4) ? (float) this->measure.RangeMilliMeter / 10 : -1;
+}
+
+//*****************************************************************************
+//
+//! Sends distance to the MSP432
+//
+//*****************************************************************************
+void LidarClass::sendMeasureLidar(float distance)
+{
+    // Start bit
+    UART.sendMessage(0);
+
+    // Increment (positive)
+    UART.sendMessage(distance);
+
+    // End bit
+    UART.sendMessage(0);
+}
+
+LidarClass Lidar;
