8c4156c76eebc8c27e13ca259b0bb125b5a65082
Pietro Bologna <106688923+bolognapietro@users.noreply.github.com>
2023-02-22 10:52:43 +0100

    Delete MSP432P401R/src directory

--- MSP432P401R/src/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/Users/pietr/Desktop/workspace_IoT/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib" -l"C:/Users/pietr/Desktop/workspace_IoT/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/lib/ccs/m4f/grlib.a"
-
